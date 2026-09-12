e436f65c013790dc17b5771f74e38efd4384d86d
Nadeem Quamar <157644952+mohdnadeemquamar@users.noreply.github.com>
2026-09-12 21:46:43 +0300

    Time: 255 ms (63.34%) | Memory: 95.4 MB (53.31%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,42 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    
+    return function(...args) {
+        const key = JSON.stringify(args);
+        
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    }
+}
+
+/** 
+ * Example usage:
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 2) // returns 4
+ * memoizedFn(2, 2) // returns 4
+ * console.log(callCount) // 1 
+ */
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
